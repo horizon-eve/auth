6f26e8c0dbf174655ee6dcdc12235d59afd0664b
Oleksandr Minaiev <oleksminaiev@gmail.com>
2021-12-19 00:36:57 -0500

    config overrides

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Horizon Auth Service
 [![CI](https://github.com/horizon-eve/auth/actions/workflows/doks.yml/badge.svg?branch=master)](https://github.com/horizon-eve/auth/actions/workflows/doks.yml)
 
-This service implements eve sso authentication flow as described here: https://developers.eveonline.com/blog/article/sso-to-authenticated-calls
+This service implements eve sso authentication flow as described here: https://docs.esi.evetech.net/docs/sso/
 
 Requires database to store auth data and manage authentication for dependent services
 
--- bin/config.js
+++ bin/config.js
@@ -1,5 +1,42 @@
 
-var config = require('../config/default-config.json');
-var environment = process.env.NODE_ENV || 'development';
-if (!config[environment]) throw 'no config for env: ' + environment
-module.exports = config[environment];
+const default_config = require('../config/default-config.json');
+const environment = process.env.NODE_ENV || 'development';
+if (!default_config[environment]) throw 'no config for env: ' + environment
+
+let effective_config
+
+if (process.env.CONFIG_OVERRIDE) {
+  let config_override = JSON.parse(process.env.CONFIG_OVERRIDE)
+  if (!config_override[environment]) throw new Error('no config override for env: ' + environment)
+  effective_config = deepMerge(default_config[environment], config_override[environment])
+} else {
+  effective_config = default_config[environment]
+}
+
+// Credits: curveball from stackoverflow.com (https://stackoverflow.com/users/7355533/curveball)
+function deepMerge (target, source) {
+  if (typeof target !== 'object' || typeof source !== 'object') return false // target or source or both ain't objects, merging doesn't make sense
+  for (const prop in source) {
+    if (!Object.prototype.hasOwnProperty.call(source, prop)) continue // take into consideration only object's own properties.
+    if (prop in target) { // handling merging of two properties with equal names
+      if (typeof target[prop] !== 'object') {
+        target[prop] = source[prop]
+      } else {
+        if (typeof source[prop] !== 'object') {
+          target[prop] = source[prop]
+        } else {
+          if (target[prop].concat && source[prop].concat) { // two arrays get concatenated
+            target[prop] = target[prop].concat(source[prop])
+          } else { // two objects get merged recursively
+            target[prop] = deepMerge(target[prop], source[prop])
+          }
+        }
+      }
+    } else { // new properties get added to target
+      target[prop] = source[prop]
+    }
+  }
+  return target
+}
+
+module.exports = effective_config
